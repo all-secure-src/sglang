968ae5d593d08504c745e890ce3694e27756b84b
apt-team <code.x.0018@gmail.com>
2024-05-31 18:21:47 +0530

    New changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 if [ "$MODEL_TYPE" = "online" ]; then
-    python3 -c "from transformers import AutoTokenizer, AutoProcessor; \
+    model_path=$(python3 -c "from transformers import AutoTokenizer, AutoProcessor; \
                 from huggingface_hub import HfFolder, snapshot_download; \
                 hf_token = '$TOKEN'; \
                 hf_folder = HfFolder(); \
                 hf_folder.save_token(hf_token); \
-                model_path = snapshot_download(repo_id='$MODEL_PATH')"
+                model_path = snapshot_download(repo_id='$MODEL_PATH'); \
+                print(model_path)")
 else
     model_path="$MODEL_PATH"
 fi
 
-python3 -m sglang.launch_server --model-path $model_path --port 8080 --mem-fraction-static 0.98 --context-length $CONTEXT_LENGTH
+python3 -m sglang.launch_server --model-path "$model_path" --port 8080 --mem-fraction-static 0.98 --context-length $CONTEXT_LENGTH
